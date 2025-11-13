af770909378d7ccf3b8cb8f72960774c11acb915
usefmahmud <usef.mahmoud@outlook.com>
2025-11-13 22:42:55 +0200

    chore: initalize the project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const LoginPage());
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login Page')),
+      body: const Center(child: Text('Welcome to the Login Page!')),
+    );
+  }
+}
