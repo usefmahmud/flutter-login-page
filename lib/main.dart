940a92f4cf5635cbd6d60251921aa89f412cb484
usefmahmud <usef.mahmoud@outlook.com>
2025-11-14 03:52:27 +0200

    refactor: replace LoginPage with RegisterPage and remove login page implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_page/pages/login.dart';
+import 'package:login_page/pages/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const LoginPage());
+    return MaterialApp(home: const RegisterPage());
   }
 }
---
+++ lib/pages/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register Page')),
+      body: const Center(child: Text('Welcome to the Register Page!')),
+    );
+  }
+}
